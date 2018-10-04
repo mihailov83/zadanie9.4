d9bd6971d397c1dbc03b926a06772cc51f347f16
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-10-04 16:04:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,9 @@
 // scripts.js
 
-var i;
-var j;
-
 function drawTree(tree) {
-    for (i = 1; i <= tree; i++) {
+    for (var i = 1; i <= tree; i++) {
         var star = '';
-        for (j = 1; j <= i; j++) {
+        for (var j = 1; j <= i; j++) {
             star += '*';
         }
         console.log(star);
